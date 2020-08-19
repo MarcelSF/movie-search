b53966dafec3ee91f30e4fbe9e875747621d36d6
Marcel Fonseca <marcelsf23br@gmail.com>
2020-08-19 11:09:44 -0300

    uncommented code

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
-// const fetchResults = (keyword) => {
-//   fetch(`http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`)
-//     .then(response => response.json())
-//     .then((data) => {
-//       console.log(data);
+// MOVIE SEARCH CODE -> GET REQUEST
 
-//       data.Search.forEach((result) => {
-//         const movie = `<li class="list-inline-item">
-//           <img src="${result.Poster}" alt="">
-//           <p>${result.Title}</p>
-//         </li>`;
+const fetchResults = (keyword) => {
+  fetch(`http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
 
-//         results.insertAdjacentHTML("beforeend", movie);
-//       });
-//     });
-// }
+      data.Search.forEach((result) => {
+        const movie = `<li class="list-inline-item">
+          <img src="${result.Poster}" alt="">
+          <p>${result.Title}</p>
+        </li>`;
 
-// const results = document.querySelector("#results");
+        results.insertAdjacentHTML("beforeend", movie);
+      });
+    });
+}
 
-// const myForm = document.querySelector('#search-movies')
+const results = document.querySelector("#results");
 
-// myForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   // see the user input(keyword)
-//   const userInput = document.querySelector('#keyword').value
-//   // Empty the list
-//   results.innerHTML = '';
-//   // Do a fetch request based on the keyword
-//   // Update the HTML
-//   fetchResults(userInput);
-// })
+const myForm = document.querySelector('#search-movies')
 
+myForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // see the user input(keyword)
+  const userInput = document.querySelector('#keyword').value
+  // Empty the list
+  results.innerHTML = '';
+  // Do a fetch request based on the keyword
+  // Update the HTML
+  fetchResults(userInput);
+})
 
+// ALGOLIA PLACES -> POST REQUEST
 
 const searchAlgoliaPlaces = (event) => {
   fetch("https://places-dsn.algolia.net/1/places/query", {
